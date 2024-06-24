37141d0084d5a82f073f904fa66b63328322c0d3
thisismegopi <gopinath10007@gmail.com>
2024-06-24 08:29:15 +0530

    added font

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -39,5 +40,10 @@ export default defineConfig(async ({ mode }) => {
         exclude: '',
       }),
     ],
+    resolve: {
+      alias: {
+        '@theme': path.resolve(__dirname, 'src/theme'),
+      },
+    },
   };
 });
